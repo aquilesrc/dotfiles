479b46920f83dd5ddeb84e32997ac386d6792095
Aquiles Rodrigues <aquiles.carneiro011@gmail.com>
2026-08-26 10:04:43 -0300

    Updated harpoon keybindings

--- .config/nvim/lua/plugins/harpoon-cfg.lua
+++ .config/nvim/lua/plugins/harpoon-cfg.lua
@@ -4,23 +4,24 @@ vim.pack.add({
 })
 
 local harpoon = require("harpoon")
+local map = vim.keymap.set
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+map("n", "<leader>a", function() harpoon:list():add() end)
+map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "<C-9>", function() harpoon:list():select(9) end)
-vim.keymap.set("n", "<C-10>", function() harpoon:list():select(10) end)
+map("n", "<leader>1", function() harpoon:list():select(1) end)
+map("n", "<leader>2", function() harpoon:list():select(2) end)
+map("n", "<leader>3", function() harpoon:list():select(3) end)
+map("n", "<leader>4", function() harpoon:list():select(4) end)
+map("n", "<leader>5", function() harpoon:list():select(5) end)
+map("n", "<leader>6", function() harpoon:list():select(6) end)
+map("n", "<leader>7", function() harpoon:list():select(7) end)
+map("n", "<leader>8", function() harpoon:list():select(8) end)
+map("n", "<leader>9", function() harpoon:list():select(9) end)
+map("n", "<leader>0", function() harpoon:list():select(10) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
+map("n", "<C-h>", function() harpoon:list():prev() end)
+map("n", "<C-l>", function() harpoon:list():next() end)
